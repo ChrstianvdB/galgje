ac4b4a089d86cdf31e614ebdfeca2a90124db869
ArminL <93372212+Arrminn@users.noreply.github.com>
2022-04-22 07:40:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welkom bij Galgje!")
+
+woorden = ("informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk")
+import random
+r = random.randint(1,10)
+gekozenwoord = woorden[r]
+print(gekozenwoord)  
