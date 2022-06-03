5dcadcf0fccdef2862f7b8e957438465d3da7284
ArminL <93372212+Arrminn@users.noreply.github.com>
2022-06-03 07:00:36 +0000

    letters blijven

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
-def game():
-  print("Welkom bij Galgje!") 
+print("Welkom bij Galgje!") 
   
-  woorden = ("informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk")
-  import random
-  r = random.randint(1,10)
-  gekozenwoord = woorden[r]
-  print(gekozenwoord)
-  aantalbeurten = 10
-  gekozenletters = []
+woorden = ("informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk")
+import random
+r = random.randint(1,10)
+gekozenwoord = woorden[r]
+print(gekozenwoord)
+aantalbeurten = 10
+gekozenletters = []
   
-  def beurt():
-    global aantalbeurten
-    print(f"Nog maar {aantalbeurten} pogingen!")
-    aantalbeurten = aantalbeurten - 1
-    gekozenletter = input("kies een letter")
+def beurt():
+  global aantalbeurten
+  print(f"Nog maar {aantalbeurten} pogingen!")
+  aantalbeurten = aantalbeurten - 1
+  gekozenletter = input("kies een letter")
   
-    streepjes = []
-    for letter in gekozenwoord:
-     if gekozenletter == letter:
-        streepjes.append(gekozenletter)
-     else:
-        streepjes.append('_')
-  
-    print(streepjes)
-    if aantalbeurten == 0:
-      print("GAME OVER")
-      c = input("Nog een keer spelen?")
-      if(c=="ja"):
-        game()
-    c = input("nog een letter?")
-    if(c=="ja"):
-      beurt()
+  streepjes = []
+  for letter in gekozenwoord:
+    if gekozenletter == letter:
+      streepjes.append(gekozenletter)
+      gekozenletters.append(gekozenletter)
+      streepjes.append(gekozenletters)
     else:
-      print("GAME OVER")
+      streepjes.append('_')
+  
+  print(streepjes)
+  if aantalbeurten == 0:
+    print("GAME OVER")
+    c = input("Nog een keer spelen?")
+    if(c=="ja"):
+      game()
+  c = input("nog een letter?")
+  if(c=="ja"):
+    beurt()
+  else:
+    print("GAME OVER")
     
   
-  beurt()
-game()
+beurt()
